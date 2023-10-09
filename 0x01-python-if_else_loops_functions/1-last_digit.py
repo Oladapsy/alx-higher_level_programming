5d929ee629c01977a9ffeaffb5a2a5acd94833c5
oladapsy <dapoodedeyi@yahoo.com>
2023-10-09 08:29:00 -0700

    get the last digit of a random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 lastdigit = abs(number) % 10
-#if number < 0:
-#   lastdigit = -(lastdigit)
+if number < 0:
+   lastdigit = -(lastdigit)
 thestring = "Last digit of {} is {}".format(number, lastdigit)
 if lastdigit > 5:
     print(f"{thestring} and is greater than 5")
