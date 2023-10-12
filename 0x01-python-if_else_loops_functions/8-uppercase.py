5bd4e80c4e589ce3fa08dbcd070671fdd6d6171f
oladapsy <dapoodedeyi@yahoo.com>
2023-10-12 01:23:08 -0700

    test

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for iterator in str:
+        temp = iterator
+        if ord(temp) >= 97 and ord(temp) >= 122:
+        ord(temp + 32)
+        print("{}".format(temp))
