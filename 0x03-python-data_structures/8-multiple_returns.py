31d752bdc73e5e18327fb1730ed7e09e3834ea72
oladapsy <dapoodedeyi@yahoo.com>
2023-10-25 14:42:23 -0700

    turple return legth and first character as a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 def multiple_returns(sentence):
     length = len(sentence)
     char = sentence[0]
-    if len == 0:
+    if length == 0:
         return 0, None
-    else:
-        return length, char
+    return length, char
