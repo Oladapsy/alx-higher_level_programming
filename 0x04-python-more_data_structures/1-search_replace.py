79d002019c96db22c9c611dc193a71aa05ae5cb0
oladapsy <dapoodedeyi@yahoo.com>
2024-01-11 23:40:10 -0800

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+        new_list = my_list[:]
+        for index, items in enumerate(new_list):
+            if items == search:
+                new_list[index] = replace
+        return new_list
