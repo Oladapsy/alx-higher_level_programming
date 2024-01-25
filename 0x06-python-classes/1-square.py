193b0e53a1c1b3a5d4b6dbe4f32d37de4cef9df8
oladapsy <dapoodedeyi@yahoo.com>
2024-01-24 20:12:36 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """A square object representation"""
+
+    def __init__(self, size):
+        """ initialize the newly created square
+        where: size equates the size of the square
+        """
+        self.__size = size
