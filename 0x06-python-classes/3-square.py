db8ef1e4fdd9682f15929dd4ba6ed063ec5c9612
oladapsy <dapoodedeyi@yahoo.com>
2024-01-24 20:55:35 -0800

    update the square object adding exception to its init value

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Representing a square"""
+
+
+class Square():
+    """A square object representation"""
+
+    def __init__(self, size=0):
+        """ initialize the newly created square
+        where: size equates the size of the square and given a default value 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """a public method that computes the area of the class obj"""
+        return (self.__size * self.__size)
