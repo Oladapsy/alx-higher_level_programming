5bb46313d21b1f3919ad9d1df92f5fe36aacfc70
oladapsy <dapoodedeyi@yahoo.com>
2024-01-24 18:32:17 -0800

    safely print integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{}".format(value))
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
+    finally:
+        return (True)
