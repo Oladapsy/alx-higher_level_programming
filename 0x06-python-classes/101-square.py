0e81bf7c45a8bef4c801b695577d544ab73e2eea
oladapsy <dapoodedeyi@yahoo.com>
2024-01-25 21:02:14 -0800

    print square

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -63,7 +63,7 @@ class Square():
         if self.__size != 0:
             [print() for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print("_", end="") for j in range(0, self.__position[0])]
+            [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for w in range(0, self.__size)]
             if i != self.__size - 1:
                 print()
