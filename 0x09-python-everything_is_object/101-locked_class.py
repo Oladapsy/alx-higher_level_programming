f124f43e2d1264fac481998d29e3fba0f0ccc838
oladapsy <dapoodedeyi@yahoo.com>
2024-01-31 14:27:32 -0800

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# work by: odedeyi oladapo olayinka
+#Lord  i thank you for the wonderful life you've given me
+""" A class with no class or obj attr...
+
+that prevents user: from creating new instance attribute
+
+except: if the new instance attribute is called first_name
+
+"""
+
+
+class LockedClass:
+    """ A class that prevent instance and only allow instance
+    with the attribute: first_name
+    """
+    __slots__ = ["first_name"]
