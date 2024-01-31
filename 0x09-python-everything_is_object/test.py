9d1f357682fcb7541f8a0e04fb0b5ad9bb8bdbc9
oladapsy <dapoodedeyi@yahoo.com>
2024-01-31 11:54:08 -0800

    18-answer.txt

---
+++ 0x09-python-everything_is_object/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+#l1 = [1, 2, 3]
+#l2 = l1
+#l1.append(4)
+#print(l2)
+
+#l1 = [1, 2, 3]
+#l2 = l1
+#l1 = l1 + [4]
+#print(l1)
+
+def increment(n):
+    n += 1
+
+a = 1
+increment(a)
+print(a)
