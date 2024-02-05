b22fefc3f9eaea6fe47b4cb3b140c17996eaad11
oladapsy <dapoodedeyi@yahoo.com>
2024-02-05 06:35:50 -0800

    replace != with is not

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,6 +7,6 @@ from a specified class
 
 def inherits_from(obj, a_class):
     """ return true if obj is from class """
-    if type(obj) != a_class and issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
