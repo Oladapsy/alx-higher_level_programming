1e108354c1ac4be7170c8f879dc3dd6a71f4f216
oladapsy <dapoodedeyi@yahoo.com>
2024-02-05 06:55:59 -0800

    a class with an exception defined

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" an new class based on 5-base_geometry.py """
+
+
+class BaseGeometry:
+    """ a defined class which is ment to be a base class """
+    def area(self):
+        """ a public instance method that raises an exception """
+        raise Exception("area() is not implemented")
