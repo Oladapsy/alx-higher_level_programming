764a24220e5041cf3e910d36e733d125c984a59f
oladapsy <dapoodedeyi@yahoo.com>
2024-02-05 06:42:41 -0800

    test

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-""" A function that returns true if the obj is an istance
-of a class that inherited directly or indirectly
-from a specified class
-"""
+""" A function that returns true if the obj is an istance"""
 
 
 def inherits_from(obj, a_class):
     """ return true if obj is from class """
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
-        return True
-    return False
+        return (True)
+    return (False)
