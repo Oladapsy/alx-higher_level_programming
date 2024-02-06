24b1aa906d044ccb799e65e41f8e29ca37b0bfce
oladapsy <dapoodedeyi@yahoo.com>
2024-02-06 09:17:14 -0800

    write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,8 +4,5 @@
 
 def write_file(filename="", text=""):
     """ The function then returns the number of char... written"""
-    line_number = 0
-    with open(filename, mode='w+', encoding='utf-8') as a_file:
-        for a_line in a_file:
-            line_number += 1
-    return line_number
+    with open(filename, mode='w', encoding='utf-8') as a_file:
+        return a_file.write(text)
