9740020c2a09c9dbbcb62f2cc81804a010e94fa1
oladapsy <dapoodedeyi@yahoo.com>
2024-02-07 10:03:49 -0800

    call in the base class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 """ an new class based on 6-base_geometry.py """
-
-
-class BaseGeometry:
-    """ a defined class which is ment to be a base class """
-    def area(self):
-        """ a public instance method that raises an exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ a public instance method that validates value """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+""" the BaseGeometry imported"""
 
 
 class Rectangle(BaseGeometry):
