af17f95356292850db6bcd1dffa91e18da5faedf
oladapsy <dapoodedeyi@yahoo.com>
2024-02-07 03:06:32 -0800

    a student class with a json defined function that retrievs the student dico

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a class that defines a student"""
+
+
+class Student:
+    """ A Student class """
+    def __init__(self, first_name, last_name, age):
+        """ the instantiation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        """ a json function that retrieves a dic.. rep of student """
+        return self.__dict__
