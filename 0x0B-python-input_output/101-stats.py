1e8a84bf51605ed72ba5b29e583a43e4dea4a974
oladapsy <dapoodedeyi@yahoo.com>
2024-02-07 05:49:19 -0800

    test

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,20 +10,20 @@ def print_stats(size, status_codes):
         status_codes (dict): the accu... count of status code
     """
     print("File size: {}".format(size))
-    for key in sorted(status_codes:
-        print("{}: {}".format(key, status_code[key]))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 if __name__ == "__main__":
     import sys
 
     size = 0
-    status_code = {}
+    status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
 
     try:
         for line in sys.stdin:
-            if count is 10:
+            if count == 10:
                 print_stats(size, status_codes)
                 count = 1
             else:
@@ -38,9 +38,9 @@ if __name__ == "__main__":
 
             try:
                 if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1
+                    if status_codes.get(line[-2], -1) == -1:
 
-                    status_codes[line[-2]] = 1
+                        status_codes[line[-2]] = 1
                 else:
                     status_codes[line[-2]] = 1
             except IndexError:
