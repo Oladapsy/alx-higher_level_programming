eadf00710b6b584e2bb71eb002893d9e2ca78e27
oladapsy <dapoodedeyi@yahoo.com>
2024-02-07 03:38:55 -0800

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" a class that defines a student based on 9-student.py"""
+
+
+class Student:
+    """ A Student class """
+    def __init__(self, first_name, last_name, age):
+        """ the instantiation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ a json function that retrieves a dic.. rep of student """
+        if type(attrs) is list and all(type(item) is str for item in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ a public method that replaces all attributes of d stud.."""
+        for key, value in json.items():
+            return setattr(self, key, value)
