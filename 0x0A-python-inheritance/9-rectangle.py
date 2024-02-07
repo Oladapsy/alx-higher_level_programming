eb3f26eacc8329b78bfddffb0eaab3411d57a853
oladapsy <dapoodedeyi@yahoo.com>
2024-02-07 10:21:15 -0800

    change 7 to 8

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """ an new class based on 6-base_geometry.py """
-BaseGeometry = __import__('8-base_geometry').BaseGeometry
-""" the BaseGeometry imported"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """ a class that inherits from Base Geometry"""
     def __init__(self, width, height):
         """ instantation with width and height  """
-        self.integer_validator("width", width)
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
