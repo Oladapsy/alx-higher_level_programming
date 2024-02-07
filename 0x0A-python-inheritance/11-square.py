f53783ed9f991c7047e1d2236637b67ed31e0ee3
oladapsy <dapoodedeyi@yahoo.com>
2024-02-07 10:41:04 -0800

    add str and area

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" importing the base class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ a square that inherits from a base class Rectangle
+    which also inherits a base class who also inherit another
+    """
+    def __init__(self, size):
+        """ initialization"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ the area of the square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ a class Square that inherits from Rectangle (9-rectangle.py)"""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__size) + "/" + str(self.__size)
+        return string
