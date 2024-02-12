7e4410a17ab826d9d6dbad01089d07074012ee62
oladapsy <dapoodedeyi@yahoo.com>
2024-02-12 10:30:08 -0800

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,8 +27,9 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ returns the list of the JSON string representation json_string"""
-        if json_string is None:
-            return ""
+        if json_string is None or len(json_string) == 0:
+            return "[]"
+        return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
