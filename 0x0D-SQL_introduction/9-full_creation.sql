22bbd04b013b00010b0d6efe28406ae9edcae224
Oladapsy <dapoodedeyi@yahoo.com>
2024-03-18 10:56:29 +0100

    debug

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,13 @@
---creates a table second_table
--- add multiple rows
+--creates a table second_tablei and add multiple row
+
 USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO "second_table" ('id', 'name', 'score') VALUES (1, "John", 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+       	name VARCHAR(256),
+       	score INT
+	);
+INSERT INTO second_table (
+	id,
+       	name, 
+	score)
+	VALUES (1, "John", 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
