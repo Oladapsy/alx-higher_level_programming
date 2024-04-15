f4be3212a1e435be8ac8e50048137aedd1244900
oladapsy <dapoodedeyi@yahoo.com>
2024-04-15 12:18:01 -0700

    test

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     db.commit()
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] == "N":
+            print(row)
     cur.close()
     db.close()
