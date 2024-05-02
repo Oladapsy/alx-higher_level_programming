0c183dd7c81405bd8864a6afc5eb4db8327074a3
Oladapsy <dapoodedeyi@yahoo.com>
2024-05-02 14:01:21 +0100

    test

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,9 +13,12 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    data = urllib.parse.urlencode(email)
+    value = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(value)
     data = data.encode('ascii')
+
     req = Request(url, data)
     with urlopen(req) as response:
-        the_page = response.read()
+        the_page = response.read().decode('utf-8')
+        print(the_page)
