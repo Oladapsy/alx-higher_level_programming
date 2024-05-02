8f1dacbfb7d7214a857a5d24988b0a7e80417294
Oladapsy <dapoodedeyi@yahoo.com>
2024-05-02 17:20:41 +0100

    test

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""The Holberton School staff evaluates candidates applying
+for a back-end position with multiple technical challenges,
+like this one:
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r_name = sys.argv[1]
+    o_name = sys.argv[2]
+    url = F"https://developer.github.com/v3/repos/{o_name}/{r_name}/commits/"
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
