1df802dfcd70299a69d7f1723c6b0585d057261c
Oladapsy <dapoodedeyi@yahoo.com>
2024-05-02 15:26:57 +0100

    debug

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,6 +9,6 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = {'email_key': sys.argv[2]}
+    email = {'email': sys.argv[2]}
     r = requests.post(url, data=email)
     print(r.text)
