ad3a9e5707d961c47676c5845b9462382a70bf40
Oladapsy <dapoodedeyi@yahoo.com>
2024-05-02 16:50:25 +0100

    ai_check

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" a Python script that takes your GitHub credentials
+(username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        user_info = response.json()
+        user_id = user_info.get('id')
+        print(user_id)
+    else:
+        print(None)
